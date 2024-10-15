7f4ac2b2d804e92ca5e8a5f9ce1846c2c6b00067
mono300genuine <monolinker31@gmail.com>
2024-10-14 22:39:58 -0700

    compare

--- README.md
+++ README.md
@@ -36,6 +36,26 @@ When you push for the first time you'll have to set the upstream
 name your repo TemplateNumber-template
 example: template 29 would be "29-template"
 
+### Compare
+
+I've introduced a new thing called Compare. 
+
+With this we can compare our video and the source video side by side in the studio.
+
+
+
+It's a file where you'll have to change the staticFile to your video.
+
+
+- create a .env file. have a look at .env.example
+- use COMPARE='x' or COMPARE='y'
+- You'll have to make the width or height in the Root.tsx file * 2 to show both next to each other
+- Put your video File in the public/examples folder
+- Change the staticFile() in Compare.tsx to the name of your video
+
+-> Adjust the timings of our video to fit the mp4
+
+
 ## Text splitter
 
 Is a component we always use for text animations.
---
+++ src/Composition/Compare.tsx
@@ -0,0 +1,43 @@
+import { ReactNode } from 'react'
+import { AbsoluteFill, OffthreadVideo, staticFile } from 'remotion'
+
+import { HEIGHT, WIDTH } from '../lib/consts'
+import Main from './Composition'
+
+export const Compare: React.FC = (props: any) => {
+  return (
+    // insert Name of your video from public/examples/ folder 
+    <CompareWithVideo example={staticFile(`/examples/${"YourTemplateHere"}.mp4`)}>
+      <Main {...props} />
+    </CompareWithVideo>
+  )
+}
+
+
+export const COMPARE = process.env.COMPARE as 'x' | 'y' | undefined
+
+export const CompareWithVideo = ({ example, children }: { example: string; children: ReactNode }) => {
+  return (
+    <AbsoluteFill style={{ overflow: 'visible', width: '100%', height: '100%' }}>
+
+     <AbsoluteFill style={{width:'1920px', height:'1080px', position:'absolute'}}>
+      {children}
+      </AbsoluteFill>
+      {COMPARE && (
+        <AbsoluteFill style={{ zIndex: 50 }}>
+          <OffthreadVideo
+            src={example}
+            onError={console.log}
+            style={{
+              marginTop: 'auto',
+              marginLeft: 'auto',
+              background: 'white',
+              height: HEIGHT,
+              width: WIDTH,
+            }}
+          />
+        </AbsoluteFill>
+      )}
+    </AbsoluteFill>
+  )
+}
